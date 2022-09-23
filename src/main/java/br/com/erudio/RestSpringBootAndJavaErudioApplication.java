b6a11490a22df687670e8626b6c84efb2f6cb77e
unknown <heron.barroso@gmail.com>
2022-09-23 17:46:54 -0300

    Start projeto devtools and web

---
+++ src/main/java/br/com/erudio/RestSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootAndJavaErudioApplication.class, args);
+	}
+
+}
